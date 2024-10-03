098b98e48ef4845733e81e3abb0e9a0fa444689b
ano-jayden <anoyakutira15022@gmail.com>
2024-10-03 15:05:55 +0200

    Delete mySite - poll temp- auth directory

--- mySite - poll temp- auth/personal/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views  
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('page1/', views.page1, name='page1'),
-    path('page2/', views.page2, name='page2'),
-]
